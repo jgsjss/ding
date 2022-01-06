6b46afd8b520b12f5e97306669964fba88d19e62
hayul2 <haeul1008@naver.com>
2022-01-06 15:34:57 +0900

    01.06 15:35
    ordermanagement1_2 작업 및 네비바 너비변경

---
+++ src/assets/menuNameData.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        name : [
+            {name : "메뉴", number: "1"},
+            {menuname : "자몽에이드", number: "2"},
+        ],
+        price: 12300,
+        isSoldOut : false,
+        isHidden : false,
+        categories1 : '메뉴',
+        categories2 : '콜드브루'
+    },
+]
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="nav_ontainer">
     <ul class="nav flex-column">
       <li class="nav-item">
         <i class="xi-cloud-o"></i>
@@ -45,17 +45,17 @@ export default {
 </script>
 
 
-<style scoped>
+<style>
 ul {list-style:none;}
-.container {
+.nav_container {
   width:100%;
   padding:30px 15px;
 }
 .nav {
   float:left;
   background:#dcd4e3;
-  padding:0 15px;
-  margin:0 30px;
+  padding:30px 15px;
+  margin:0 10px;
 }
 .nav-item {
   text-align: center;
@@ -76,107 +76,5 @@ ul {list-style:none;}
   text-decoration: none;
 }
 
-/* media query */
-@media ( min-width : 768px){
-  .container {
-    width:100%;
-    padding:30px 15px;
-  }
-  .nav {
-    float:left;
-    background:#dcd4e3;
-  }
-  .nav-item {
-    text-align: center;
-    padding:15px 0;
-  }
-  .nav-item > .xi-cloud-o {
-    border:1px dashed #c6a6e2;
-    border-radius: 100%;
-    padding:5px;
-    color:#fff;
-  }
-  .nav-link {
-    color:#997fb5;
-    text-decoration: none;
-    font-size:10px;
-  }
-  .nav-link:hover {
-    color:#997fb5;
-    text-decoration: none;
-  }
-  a {
-    text-decoration: none;
-  }
 
-}
-@media ( min-width : 992px) {
-  .container {
-    width:100%;
-    padding:30px 15px;
-  }
-  .nav {
-    float:left;
-    background:#dcd4e3;
-  }
-  .nav-item {
-    text-align: center;
-    padding:15px 0;
-  }
-  .nav-item > .xi-cloud-o {
-    border:1px dashed #c6a6e2;
-    border-radius: 100%;
-    padding:5px;
-    color:#fff;
-  }
-  .nav-link {
-    color:#997fb5;
-    text-decoration: none;
-    font-size:14px;
-  }
-  .nav-link:hover {
-    color:#997fb5;
-    text-decoration: none;
-  }
-}
-@media ( min-width : 1200px) {
-  .container {
-    width:100%;
-    padding:30px 15px;
-  }
-  .nav {
-    float:left;
-    background:#dcd4e3;
-  }
-  .nav-item {
-    text-align: center;
-    padding:15px 0;
-  }
-  .nav-item > .xi-cloud-o {
-    border:1px dashed #c6a6e2;
-    border-radius: 100%;
-    padding:5px;
-    color:#fff;
-  }
-  .nav-link {
-    color:#997fb5;
-    text-decoration: none;
-    font-size:16px;
-  }
-  .nav-link:hover {
-    color:#997fb5;
-    text-decoration: none;
-  }
-}
-.black-bg {
-  width: 100%; height:100%;
-  background: rgba(0,0,0,0.5);
-  position: fixed; padding: 20px;
-  z-index: 9999;
-}
-.white-bg {
-  width: 100%; background: white;
-  border-radius: 8px;
-  padding: 20px;
-}
 </style>
