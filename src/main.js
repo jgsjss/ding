948517f66f75043e9c336aa069d61186107583fd
dcp <eetadev01@gmail.com>
2022-01-06 16:51:16 +0900

    mixins

--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import mitt from 'mitt'
-import axios from 'axios'
-import VueSession from 'vue-session'
+import mixins from './mixins'
 
 const emitter = mitt()
 const app = createApp(App)
 app.config.globalProperties.emitter = emitter
-// app.use(VueSession)
-// app.use(axios)
+app.mixin(mixins)
 
 app.use(store).use(router).mount('#app')
