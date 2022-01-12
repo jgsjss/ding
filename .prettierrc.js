3795dd9fc20da69f3fa2c739d2e8262ad6fd125b
dcp <eetadev01@gmail.com>
2022-01-12 09:58:38 +0900

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  quoteProps: 'as-needed',
+  printWidth: 120,
 };
-
