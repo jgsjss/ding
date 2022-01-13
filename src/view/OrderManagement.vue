b79f9bcd6677437f6818b9205b7780048f440aaa
hayul2 <haeul1008@naver.com>
2022-01-13 14:23:48 +0900

    01.13 14:23
    ordermanagement 내부 css수정

--- src/view/OrderManagement.vue
+++ src/view/OrderManagement.vue
@@ -11,12 +11,14 @@
         <i class="xi-bell"></i>
       </div>
       </div>
+      <hr />
       <div class="component_bar">
         <router-link to="/ordermanagement/neworderlistone"><span class="component_name first">신규주문{{}}</span></router-link>
         <router-link to="/ordermanagement/preparinglistone"><span class="component_name">준비중{{}}</span></router-link>
         <router-link to="/ordermanagement/completedorder"><span class="component_name">완료주문{{}}</span></router-link>
        <router-link to="/ordermanagement/orderlist"><span class="component_name">주문내역</span></router-link>
       </div>
+      <hr />
 
         <!-- <div class="list_wrap">
           <router-link to="/OrderManagement/1"><i class="xi-list-square list"></i></router-link>
