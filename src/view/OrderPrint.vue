e1f54812a48e40ca22770c743ee25221d49e2f77
hayul2 <haeul1008@naver.com>
2022-01-17 08:39:57 +0900

    01.17 08:39
    ordermanagement 부분 간격 수정

--- src/view/OrderPrint.vue
+++ src/view/OrderPrint.vue
@@ -90,6 +90,26 @@
                 </div>
             </div>
             </div>
+            <!--/////주문취소 모달 끝/////-->
+            <!--/////취소 확인 모달창/////-->
+            <!-- <div class="modal" tabindex="-1">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div> -->
+            <!--/////취소 확인 모달창/////-->
           </div>
         </div>
       </div>
@@ -139,7 +159,7 @@ import * as Printjs from "print-js";
 export default {
     data() {
         return {
-            modal: false,
+          showModal: false,
         }
     },
    method: {
