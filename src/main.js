d559ce365c47716fc0f2aded47685f1a8930c797
jgsjss9854 <87510168+jgsjss@users.noreply.github.com>
2022-01-25 16:56:06 +0900

    commit
    11

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import axios from 'axios'
 import store from './store'
 import mitt from 'mitt'
 import mixins from './mixins'
-
+import vuesweetalert2 from 'vue-sweetalert2'
+import "./assets/sweetalert2.min.css"
 
 const emitter = mitt();
 const app = createApp(App);
 app.config.globalProperties.emitter = emitter;
 app.config.globalProperties.axios = axios;
 app.mixin(mixins)
+app.use(vuesweetalert2)
 app.use(store).use(router).mount('#app')
