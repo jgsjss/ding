5e7e76ed51e9e0969e2c0fbac2b50771c129997d
dcp <eetadev01@gmail.com>
2022-02-09 08:45:47 +0900

    prettier single quote to double

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   trailingComma: 'es5',
   tabWidth: 2,
   semi: true,
-  singleQuote: true,
+  singleQuote: false,
   bracketSameLine: false,
   arrowParens: 'always',
   quoteProps: 'as-needed',
