98ef6b5875291ded58321d7914ec15ea7c0614c1
dcp <eetadev01@gmail.com>
2022-02-08 15:02:29 +0900

    salesManageOne calcprice

--- src/view/SalesManagement/SalesManagementOne.vue
+++ src/view/SalesManagement/SalesManagementOne.vue
@@ -1,33 +1,32 @@
 <template>
-<div class="month_container">
-  <div class="month_wrap">
-    <div class="month_left">
-      <div class="month_left_wrap">
-          <Datepicker v-model="month" monthPicker modeHeight="120" locale="ko" />
-      </div>
-    <div class="month_list_wrap">
-      <div class="m_list_wrap1">
-        <div class="month_text_left">총 매출금액</div>
-        <div class="month_text_left">총 주문건수</div>
-        <div class="month_text_left">총 주문메뉴</div>
-      </div>
+  <div class="month_container">
+    <div class="month_wrap">
+      <div class="month_left">
+        <div class="month_left_wrap">
+          <Datepicker v-model="month" monthPicker modeHeight="120" locale="ko"/>
+        </div>
+        <div class="month_list_wrap">
+          <div class="m_list_wrap1">
+            <div class="month_text_left">총 매출금액</div>
+            <div class="month_text_left">총 주문건수</div>
+            <div class="month_text_left">총 주문메뉴</div>
+          </div>
 
-      <div class="m_list_wrap2">
-        <button type="button" @click="calcTotalPrice">calc</button>
-        <div class="month_text_right">{{totalPrice}}원</div>
-        <div class="month_text_right">{{}}832건</div>
-        <div class="month_text_right"> {{}}1452개</div>
+          <div class="m_list_wrap2">
+            <div class="month_text_right">{{ totalPrice }}원</div>
+            <div class="month_text_right">{{}}832건</div>
+            <div class="month_text_right"> {{}}1452개</div>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-<div class="month_right">
-  <div class="sales_sort_btn_wrap">
-  <select class="sales_sort_btn">
-    품절해제
-    <option class="log_check_Box" >최신순</option>
-    <option class="log_check_Box" >과거순</option>
-  </select>
-  </div>
+      <div class="month_right">
+        <div class="sales_sort_btn_wrap">
+          <select class="sales_sort_btn">
+            품절해제
+            <option class="log_check_Box">최신순</option>
+            <option class="log_check_Box">과거순</option>
+          </select>
+        </div>
         <table class="table">
           <thead class="sales_head">
           <tr class="sales_title">
@@ -41,73 +40,83 @@
           </thead>
           <tbody>
           <tr v-for="(a, i) in $store.state.SalesData" :key="i">
-            <td class="sales_data"><router-link to="../ordermanage/orderprint">{{ $store.state.SalesData[i].orderdata }}</router-link></td>
+            <td class="sales_data">
+              <router-link to="../ordermanage/orderprint">{{ $store.state.SalesData[i].orderdata }}</router-link>
+            </td>
             <td class="sales_data">{{ $store.state.SalesData[i].watingnum }}</td>
-            <td class="sales_data">{{ $store.state.SalesData[i].division }} </td>
+            <td class="sales_data">{{ $store.state.SalesData[i].division }}</td>
             <td class="sales_data">{{ $store.state.SalesData[i].content }}</td>
             <td class="sales_data">{{ $store.state.SalesData[i].price }}</td>
             <td class="sales_data">{{ $store.state.SalesData[i].etc }}</td>
           </tr>
           </tbody>
         </table>
-      <div class="btn-cover">
-      <button :disabled="pageNum === 0" @click="prevPage" class="page-btn">
-        <i class="xi-angle-left"></i>
-      </button>
-      <span class="page-count">{{ pageNum + 1 }} / {{ pageCount }} </span>
-      <button :disabled="pageNum >= pageCount - 1" @click="nextPage" class="page-btn">
-        <i class="xi-angle-right"></i>
-      </button>
-    </div>
+        <div class="btn-cover">
+          <button :disabled="pageNum === 0" @click="prevPage" class="page-btn">
+            <i class="xi-angle-left"></i>
+          </button>
+          <span class="page-count">{{ pageNum + 1 }} / {{ pageCount }} </span>
+          <button :disabled="pageNum >= pageCount - 1" @click="nextPage" class="page-btn">
+            <i class="xi-angle-right"></i>
+          </button>
+        </div>
 
-</div>
-</div>
-</div>
+      </div>
+    </div>
+  </div>
 
 </template>
 <script>
-import { ref } from 'vue';
+import { ref } from 'vue'
+import { forEach } from 'lodash'
+import _ from 'lodash'
 
-  export default {
-  components:{
-  },
-  data() {
+export default {
+  components: {},
+  data () {
     return {
-        pageNum: 0,
-      totalPrice:0,
+      pageNum: 0,
+      totalPrice: 0,
     }
   },
   methods: {
-      nextPage() {
-        this.pageNum += 1;
-      },
-      prevPage() {
-        this.pageNum -= 1;
-      },
-      calcTotalPrice() {
-        let tempPrice = 0;
-        // for (let index = 1; index < this.$store.state.SalesData.length; index++) {
-        //   tempPrice += this.$store.state.SalesData.price[index];
-        // }
-        this.totalPrice=tempPrice
-        console.log(this.$store.state.SalesData.length)
-        console.log(this.$store.state.SalesData)
-        // console.log(tempPrice)
-      }
+    nextPage () {
+      this.pageNum += 1
     },
-    setup() {
-        const month = ref({ 
-            month: new Date().getMonth(),
-            year: new Date().getFullYear()
-        });
-        
-        return {
-            month,
-        }
-    }   
+    prevPage () {
+      this.pageNum -= 1
+    },
+    calcTotalPrice (object) {
 
+      let sum = 0
+      _.forEach(_.map(object, 'price'), function (val, key) {
+        sum += val
+      })
+      this.totalPrice = sum
+    }
+
+  },
+  setup () {
+    const month = ref({
+      month: new Date().getMonth(),
+      year: new Date().getFullYear()
+    })
+
+    return {
+      month,
+    }
+  },
+  computed: {
+    pageCount () {
+      return Math.ceil(this.$store.state.SalesData.length / 10)
+    },
+
+  },
+  created () {
+    this.calcTotalPrice(this.$store.state.SalesData)
   }
 
+}
 
 
 </script>
