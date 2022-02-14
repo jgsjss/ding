c488f5f071a8da9c17ffd50e00b0b7a8c88029aa
hayul2 <haeul1008@naver.com>
2022-02-14 12:49:04 +0900

    02.14 12:49
    ㅇㅇ

--- src/view/member/UpdateBizMember.vue
+++ src/view/member/UpdateBizMember.vue
@@ -4,6 +4,18 @@
       <h4 class="update_title">비밀번호 수정</h4>
         <!-- PW1 -->
         <div>
+          <h3 className="join_title"><label>현재 비밀번호</label></h3>
+          <span className="box int_pass">
+                            <input type="password" id="pswd2-1" className="int" maxLength="16"
+                                   placeholder="현재 비밀번호를 입력해 주세요." v-model="userPw1" ref="" @change="checkPW" @focus="checkFlag = false">
+            <!-- <span>유효하지 않은 비밀번호 입니다.</span> -->
+            <!-- <img src="" id="pswd1_img1" class="pswdImg"> -->
+            <span class="error_next_box1" id="pw1Msg" style aria-live="assertive">필수 정보 입니다.</span>  
+                        </span>        
+          <span className="error_next_box"></span>
+        </div>
+        <!-- PW2 -->
+        <div>
           <h3 className="join_title"><label>비밀번호</label></h3>
           <span className="box int_pass">
                             <input type="password" id="pswd2-1" className="int" maxLength="16"
@@ -13,9 +25,8 @@
             <span class="error_next_box1" id="pw1Msg" style aria-live="assertive">필수 정보 입니다.</span>  
                         </span>        
           <span className="error_next_box"></span>
-          
         </div>
-        <!-- PW2 -->
+        <!-- PW3 -->
         <div>
           <h3 className="join_title"><label>비밀번호 재확인</label></h3>
           <span className="box int_pass_check">
