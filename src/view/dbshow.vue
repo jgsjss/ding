30182c56681b676a64eeedbce6696a047554dc11
dcp <eetadev01@gmail.com>
2022-02-14 09:47:00 +0900

    Update dbshow.vue

--- src/view/dbshow.vue
+++ src/view/dbshow.vue
@@ -6,9 +6,8 @@
             <th scope="col">아이디</th>
             <th scope="col">패스워드</th>
             <th scope="col">이름</th>
-            <th scope="col">도로명주소</th>
-            <th scope="col">(구)주소</th>
-            <th scope="col">건물명</th>
+            <th scope="col">주소 1</th>
+            <th scope="col">주소 2</th>
             <th scope="col">e-mail</th>
             <th scope="col">연락처</th>
             <th scope="col">가게이름</th>
@@ -20,7 +19,6 @@
             <td>{{ user.pw }}</td>
             <td>{{ user.username }}</td>
             <td>{{ user.bizaddr1 }}</td>
-            <td>{{ user.bizaddr2 }}</td>
             <td>{{ user.bizaddr2 }}</td>
             <td>{{ user.mgemail }}</td>
             <td>{{ user.phnum }}</td>
