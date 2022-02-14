b9126e295f638d60dbcf26dd4a89545300af922d
dcp <eetadev01@gmail.com>
2022-02-14 09:22:02 +0900

    user for cycle

--- src/view/dbshow.vue
+++ src/view/dbshow.vue
@@ -1,10 +1,36 @@
 <template>
-<h1>dbshow페이지</h1>
+  <h1>dbshow페이지</h1>
+  <table>
+    <tr v-for="user in resultset" key="user">
+      <td>{{ user.userid }}</td>
+      <td>{{ user.pw }}</td>
+      <td>{{ user.username }}</td>
+      <td>{{ user.bizaddr1 }}</td>
+      <td>{{ user.bizaddr2 }}</td>
+      <td>{{ user.mgemail }}</td>
+      <td>{{ user.phnum }}</td>
+      <td>{{ user.shopname }}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'dbshow'
+  name: 'dbshow',
+  data () {
+    return {
+      resultset: []
+    }
+  },
+  created () {
+    axios.post('/member/db')
+        .then(response => {
+          this.resultset = response.data
+          console.log(this.resultset)
+        })
+  },
 }
 </script>
 
