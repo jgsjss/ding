86a007f3d3fd0211616aab8df8ee2f7462173df1
hayul2 <haeul1008@naver.com>
2022-02-18 13:36:22 +0900

    02.18 13:36
    ....i hate

--- src/assets/CategoryOptionData.js
+++ src/assets/CategoryOptionData.js
@@ -1,18 +1,26 @@
 export default [
     {
         catename: '오늘의 커피',
-        menuname: '바닐라 라떼, 코코넛 라떼,딸기 라떼'
+        menuname: '바닐라 라떼, 코코넛 라떼,딸기 라떼',
+        dessert: '디저트 추가선택',
+        dessertname: '쿠키프라페 마카롱,유니콘 마카롱,돼지바 마카롱'
     },
     {
         catename: '딩동 시그니처',
-        menuname: '진짜 진한 리얼 바닐라 딥, 진짜 모히또'
+        menuname: '진짜 진한 리얼 바닐라 딥, 진짜 모히또',
+        dessert:'원두변경',
+        dessertname:'케냐, AAA, 다크, 브라질'
     },
     {
         catename: '콜드브루',
-        menuname: '바닐라 라떼, 코코넛 라떼,딸기 라떼'
+        menuname: '바닐라 라떼, 코코넛 라떼,딸기 라떼',
+        dessert: '당도선택',
+        dessertname:'100%, 70%, 50%, 30%, 10%, 0%'
     },
     {
         catename: '오늘의 커피',
-        menuname: '바닐라 라떼, 코코넛 라떼,딸기 라떼'
+        menuname: '바닐라 라떼, 코코넛 라떼,딸기 라떼',
+        dessert:' 휘핑 선택',
+        dessertname:' 휘핑 없이, 휘핑 조금, 휘핑 기본, 휘핑 많이'
     },
 ]
--- src/view/MenuManagement/MenuAdd.vue
+++ src/view/MenuManagement/MenuAdd.vue
@@ -67,14 +67,52 @@
             </div>
           </div>
         </label>
+        <!--옵션설정-->
         <label class="menuadd_label">옵션
           <button type="button" class="menuadd_edit_btn"  data-bs-toggle="collapse" data-bs-target="#option_edit" aria-expanded="false" aria-controls="option_edit">옵션설정</button>
-            <div class="category_option_bind">{{}}연결된 옵션 없음</div>          
-          <div class="collapse  category_option_wrap" id="option_edit">
+            <div class="option_edit_bind">{{}}연결된 옵션 없음</div>
+          <div class="collapse option_edit_wrap" id="option_edit">
             <div class="card card-body">
-              Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+              <div class="option_edit_add_wrap">
+                <div class="option_edit_add_left">
+                  <div class="option_edit_add_title_wrap">
+                  <h4 class="option_edit_add_title">옵션 설정</h4>
+                  <p class="option_edit_add_text">- 메뉴와 연결되는 옵션을 설정할 수 있습니다.</p>
+                  <p class="option_edit_add_text">- 삭제를 누르시면 메뉴와의 연결이 끊어집니다.</p>
+                  <p class="option_edit_add_text">- 왼쪽의 아이콘을 잡고 드래그하여 순서변경이 가능합니다.</p>
+                  </div>
+                  <div class="option_edit_add_btn_wrap">
+                    <button type="button" class="option_edit_add_btn">+옵션 연결</button>
+                  </div>
+                    <draggable class="dragArea list-group w-full" :list="list" @change="log">
+                      <div
+                        class="list-group-item bg-gray-300 m-1 p-3 rounded-md text-center menuconnect_drag_text"
+                        v-for="element in list"
+                        :key="element.name"
+                      >
+                        {{ element.name }}
+                      </div>
+                    </draggable>
+                  <div class="option_edit_add_btn_wrap">
+                    <button type="button" class="option_edit_add_btn02">저장</button>
+                  </div>
+
+                </div>
+                <div class="option_edit_add_right">
+                  <div class="option_edit_add_title_wrap">
+                  <h4 class="option_edit_add_title">카테고리 연결</h4>
+                  </div>
+                  <button type="button" v-for="(a, i) in $store.state.CategoryOptionData" :key="i" class="option_edit_name">
+                    {{ $store.state.CategoryOptionData[i].dessert }}
+                    <p>{{ $store.state.CategoryOptionData[i].dessertname }}</p>
+                  </button>
+                    <div class="option_edit_add_btn_wrap">
+                      <button type="button" class="option_edit_add_btn02">(2개선택){{}}연결</button>
+                    </div>                  
+                </div>
+              </div>
             </div>
-          </div>          
+          </div>         
         </label>
       </div>
     </div>
@@ -82,11 +120,53 @@
       <button type="button" class="menuadd_btn">메뉴추가</button>
     </div>  
   </div>
+
 </template>
 
 <script>
+  import { defineComponent } from 'vue'
+  import { VueDraggableNext } from 'vue-draggable-next'
 export default {
-
+    components: {
+      draggable: VueDraggableNext,
+    },
+      data() {
+      return {
+        enabled: true,
+        list: [
+          { name: 'HOT/ICED', id: 1 },
+          { name: '샷추가-기본2샷', id: 2 },
+          { name: '시럽추가선택', id: 3 },
+          { name: '추가 옵션', id: 4 },
+        ],
+        dragging: false,
+      }
+    },
+    methods: {
+        log(event) {
+        console.log(event)
+      },
+        handleChange() {
+        console.log('changed');
+    },
+        inputChanged(value) {
+        this.activeNames = value;
+    },
+        getComponentData() {
+            return {
+                on: {
+                change: this.handleChange,
+                input: this.inputChanged
+                },
+                attrs:{
+                wrap: true
+                },
+                props: {
+                value: this.activeNames
+                }
+            };
+    }
+    },
 }
 </script>
 
