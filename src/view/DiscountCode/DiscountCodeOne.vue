c68dbe68eabd1ca7f787cc68eb293a840dafe221
hayul2 <haeul1008@naver.com>
2022-02-23 14:59:27 +0900

    02.23 14:59
    ㅇㅇ

--- src/view/DiscountCode/DiscountCodeOne.vue
+++ src/view/DiscountCode/DiscountCodeOne.vue
@@ -9,20 +9,67 @@
           <div class="proceeding_btn_wrap02">
               <button type="submit" class="proceeding_header_right_btn" @click="discountOpen=!discountOpen">+코드추가</button>
           </div>
-          <div class="proceeding_modal_wrap" v-if=" discountOpen == true">
+          <!--할인추가 모달-->
+          <!-- <div class="proceeding_modal_wrap" v-if=" discountOpen == true">
             <div class="proceeding_modal_sub_wrap">
               <h4 class="proceeding_modal_title">할인코드 추가</h4>
               <div class="discount_box_wrap">
-                <button type="file" 
+                <input type="file" 
                         class="proceeding_modal_btn" 
-                        accept=".csv, appliction/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel">할인코드<br>일괄등록<br>(엑셀파일)</button>
-                <button type="file" 
+                        id="excelUpload" @change="importExcel" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" placeholder="업로드">할인코드<br>일괄등록<br>(엑셀파일)
+                <input type="file" 
                         class="proceeding_modal_btn" 
-                        accept=".csv, appliction/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel">할인코드<br>개별등록<br>&nbsp</button>
+                        id="excelUpload" @change="importExcel" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet">할인코드<br>개별등록<br>&nbsp
               </div>  
             </div>
-          </div>
+          </div> -->
       </div>
+      
+      <div class="proceeding_modal_container" v-show="discountOpen">
+        <div class="proceeding_modal_container01">
+          <h5 class="proceeding_modal_title">할인코드 추가(대량등록)</h5>
+          <p class="proceeding_modal_sub_title">- 할인코드는 발급 후 수정이 불가합니다.</p>
+          <p class="proceeding_modal_sub_title">- 사용 또는 기간 만료 시 자동으로 만료됩니다.</p>
+          <p class="proceeding_modal_sub_title">- 등록해야하는 쿠폰이 많은 경우 엑셀파일을 사용하여 일괄등록이 가능합니다.</p>
+          <p class="proceeding_modal_sub_title">- 아래 양식을 다운로드하여 작성하신후 파일을 업로드 해주세요.</p>  
+          <div class="proceeding_modal_input_wrap">   
+          <input type="file" class="proceeding_modal_input" placeholder="파일추가">
+          </div>   
+          <div>
+          <p class="proceeding_modal_sub_title">* 지원하는 파일 형식:</p>
+          <p class="proceeding_modal_sub_title">* 파일이 양식과 맞지 않을 경우 추가되지 않습니다.</p>
+          </div>
+          <div class="proceeding_modal_btn_wrap">
+          <button type="button" class="proceeding_modal_btn">할인코드 추가</button>
+          </div>
+        </div>
+        <div class="proceeding_modal_container02">
+          <h5 class="proceeding_modal_title">할인코드 추가(개별등록)</h5>
+          <p class="proceeding_modal_sub_title">- 할인코드는 발급 후 수정이 불가합니다.</p>
+          <p class="proceeding_modal_sub_title">- 기간이 지나거나 사용하면 자동으로 종료처리 됩니다.</p>
+          <form>
+            <label for="" class="proceeding_modal_label form-label"> 할인코드
+              <input type="text" placeholder="9-숫자를 입력해주세요(최대10자리)" class="proceeding_modal_input">
+            </label>
+            <label for="" class="proceeding_modal_label">할인코드명
+              <input type="text" placeholder="ex) 1주년 기념 스페셜 할인" class="proceeding_modal_input">
+            </label>
+            <label for="" class="proceeding_modal_label">할인항목
+              <input type="radio">&nbsp결제금액&nbsp<input type="text" placeholder="ex) 1주년 기념 스페셜 할인">&nbsp%할인
+            </label>
+            <label for="" class="proceeding_modal_label">
+              <input type="radio">&nbsp결제금액&nbsp<input type="text" placeholder="ex) 1주년 기념 스페셜 할인">&nbsp%할인
+            </label>
+            <label for="" class="proceeding_modal_label">사용가능횟수
+              <input type="radio">&nbsp다회(제한없음)
+              <input type="radio">&nbsp1회 (코드당 1번)
+            </label>
+            <Datepicker v-model="date" range datePicker modeHeight="120" locale="ko" weekStart="0" :enableTimePicker="false" value="2022-02-08"/>
+
+          </form>
+        </div>
+      </div>
+
     <div class="proceeding_list_wrap">
       <table class="proceeding_table">
         <thead class="proceeding_head">
@@ -76,6 +123,23 @@ export default {
       discountOpen:false,
       XLSX:'',
     }
+  },
+  methods: {
+// importExcel(event) {
+//     const file = event.target.files[0];
+//     let reader = new FileReader();
+//     reader.onload = (e) => {
+//         let data = reader.result;
+//         // let data = e.target.result;
+//         let workbook = XLSX.read(data, {type: 'binary'});
+//         workbook.SheetNames.forEach(sheetName => {
+//             const roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+//             this.items.push(roa);
+//         });
+//     };
+//     reader.readAsBinaryString(file);
+// },
+
   }
 }
 </script>
