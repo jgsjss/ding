806c0f166732a5b01ae30e9c8342556e6f4229e2
liam <eetadev01@gmail.com>
2022-03-03 19:02:16 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,8 @@
-<<<<<<< Updated upstream
-// const target = "http://192.168.0.110:5000";
-const target = "http://39.113.242.19:5000";
 
-=======
-const target = "http://39.113.242.19:5000";
-// "http://58.239.156.241:5000"
->>>>>>> Stashed changes
+// const target = "http://192.168.0.110:5000";
+const target = "http://localhost:5000";
+
+
 module.exports = {
   devServer: {
     port: 8080,
