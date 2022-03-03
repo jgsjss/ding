7879acdc6cac5d1570ef142d90d9361272ac7205
jks123 <jgsjss9854@naver.com>
2022-03-03 15:54:16 +0900

    fgyuj

--- src/view/MenuManagement/MenuAdd.vue
+++ src/view/MenuManagement/MenuAdd.vue
@@ -7,33 +7,34 @@
     <div class="menuadd_box_wrap">
       <div class="menuadd_wrap">
         <form>
-        <label class="menuadd_label form-label">* 메뉴명
-          <input type="text" class="menuadd_input form-control" placeholder="예) 딩동아메리카노">
-        </label>
-        <label class="menuadd_label form-label">* 가격
-          <input type="text" class="menuadd_input form-control">
-        </label>
-        <label class="menuadd_label form-label">* 설명
-          <textarea type="text" class="menuadd_input form-control" placeholder="예) 딩동커피만의 로스팅방식으로 만들어낸 아메리카노"></textarea>
-        </label>
-        <label class="menuadd_label form-label"> 이미지
-          <input 
-            type="file" 
-            class="menuadd_img_btn form-control"
-            @change="upload"
-            multiple accept="image/*"
-            id="file"
+          <label class="menuadd_label form-label">* 메뉴명
+            <input type="text" class="menuadd_input form-control" placeholder="예) 딩동아메리카노">
+          </label>
+          <label class="menuadd_label form-label">* 가격
+            <input type="text" class="menuadd_input form-control">
+          </label>
+          <label class="menuadd_label form-label">* 설명
+            <textarea type="text" class="menuadd_input form-control"
+                      placeholder="예) 딩동커피만의 로스팅방식으로 만들어낸 아메리카노"></textarea>
+          </label>
+          <label class="menuadd_label form-label"> 이미지
+            <input
+                type="file"
+                class="menuadd_img_btn form-control"
+                @change="upload"
+                multiple accept="image/*"
+                id="file"
             >
-        </label>
-        <div class="menuadd_img_input form-label">
-        <img :src="image" alt="메뉴이미지" class="popupImageItem" style="width:200px; text-align:center;" >
-        </div>
-        <label class="form-label">메뉴공개&nbsp
-          <input type="radio" name="menu_add_radio" class="menuadd_radio">&nbsp공개
-        </label>
-        <label class="form-label">&nbsp
-          <input type="radio" name="menu_add_radio" class="menuadd_radio">&nbsp숨김 (딩동오더에 노출 안됨)
-        </label>
+          </label>
+          <div class="menuadd_img_input form-label">
+            <img :src="image" alt="메뉴이미지" class="popupImageItem" style="width:200px; text-align:center;">
+          </div>
+          <label class="form-label">메뉴공개&nbsp
+            <input type="radio" name="menu_add_radio" class="menuadd_radio">&nbsp공개
+          </label>
+          <label class="form-label">&nbsp
+            <input type="radio" name="menu_add_radio" class="menuadd_radio">&nbsp숨김 (딩동오더에 노출 안됨)
+          </label>
         </form>
       </div>
       <!--카테고리설정 및 옵션설정 창-->
@@ -46,14 +47,15 @@
               <div class="category_option_add_wrap">
                 <div class="category_option_add_left">
                   <div class="category_option_add_title_wrap">
-                  <h4 class="category_option_add_title">카테고리 설정</h4>
-                  <p class="category_option_add_text">- 메뉴와 연결되는 카테고리를 설정할 수 있습니다.</p>
-                  <p class="category_option_add_text">- 삭제를 누르시면 메뉴와의 연결이 끊어집니다.</p>
+                    <h4 class="category_option_add_title">카테고리 설정</h4>
+                    <p class="category_option_add_text">- 메뉴와 연결되는 카테고리를 설정할 수 있습니다.</p>
+                    <p class="category_option_add_text">- 삭제를 누르시면 메뉴와의 연결이 끊어집니다.</p>
                   </div>
                   <div class="category_option_add_btn_wrap">
                     <button type="button" class="category_option_add_btn">+카테고리 연결</button>
                   </div>
-                  <button type="button" v-for="(a, i) in $store.state.CategoryOptionData" :key="i" class="category_option_name">
+                  <button type="button" v-for="(a, i) in $store.state.CategoryOptionData" :key="i"
+                          class="category_option_name">
                     {{ $store.state.CategoryOptionData[i].catename }}
                     <p>{{ $store.state.CategoryOptionData[i].menuname }}</p>
                   </button>
@@ -64,15 +66,16 @@
                 </div>
                 <div class="category_option_add_right">
                   <div class="category_option_add_title_wrap">
-                  <h4 class="category_option_add_title">카테고리 연결</h4>
+                    <h4 class="category_option_add_title">카테고리 연결</h4>
                   </div>
-                  <button type="button" v-for="(a, i) in $store.state.CategoryOptionData" :key="i" class="category_option_name">
-                    {{ $store.state.CategoryOptionData[i].catename }}
-                    <p>{{ $store.state.CategoryOptionData[i].menuname }}</p>
+                  <button type="button" v-for="(a, i) in cgData" :key="i"
+                          class="category_option_name">
+                    {{ a[i].pdcategory }}
+                    <p>{{ a[i].description }}</p>
                   </button>
-                    <div class="category_option_add_btn_wrap">
-                      <button type="button" class="category_option_add_btn02">(2개선택){{}}연결</button>
-                    </div>                  
+                  <div class="category_option_add_btn_wrap">
+                    <button type="button" class="category_option_add_btn02">(2개선택){{}}연결</button>
+                  </div>
                 </div>
                 
               </div>
@@ -81,30 +84,32 @@
         </label>
         <!--옵션설정-->
         <label class="menuadd_label">옵션
-          <button type="button" class="menuadd_edit_btn"  data-bs-toggle="collapse" data-bs-target="#option_edit" aria-expanded="false" aria-controls="option_edit">옵션설정</button>
-            <div class="option_edit_bind">{{}}연결된 옵션 없음</div>
+          <button type="button" class="menuadd_edit_btn" data-bs-toggle="collapse" data-bs-target="#option_edit"
+                  aria-expanded="false" aria-controls="option_edit">옵션설정
+          </button>
+          <div class="option_edit_bind">{{}}연결된 옵션 없음</div>
           <div class="collapse option_edit_wrap" id="option_edit">
             <div class="card card-body">
               <div class="option_edit_add_wrap">
                 <div class="option_edit_add_left">
                   <div class="option_edit_add_title_wrap">
-                  <h4 class="option_edit_add_title">옵션 설정</h4>
-                  <p class="option_edit_add_text">- 메뉴와 연결되는 옵션을 설정할 수 있습니다.</p>
-                  <p class="option_edit_add_text">- 삭제를 누르시면 메뉴와의 연결이 끊어집니다.</p>
-                  <p class="option_edit_add_text">- 왼쪽의 아이콘을 잡고 드래그하여 순서변경이 가능합니다.</p>
+                    <h4 class="option_edit_add_title">옵션 설정</h4>
+                    <p class="option_edit_add_text">- 메뉴와 연결되는 옵션을 설정할 수 있습니다.</p>
+                    <p class="option_edit_add_text">- 삭제를 누르시면 메뉴와의 연결이 끊어집니다.</p>
+                    <p class="option_edit_add_text">- 왼쪽의 아이콘을 잡고 드래그하여 순서변경이 가능합니다.</p>
                   </div>
                   <div class="option_edit_add_btn_wrap">
                     <button type="button" class="option_edit_add_btn">+옵션 연결</button>
                   </div>
-                    <draggable class="dragArea list-group w-full" :list="list" @change="log">
-                      <div
+                  <draggable class="dragArea list-group w-full" :list="list" @change="log">
+                    <div
                         class="list-group-item bg-gray-300 m-1 p-3 rounded-md text-center menuconnect_drag_text"
                         v-for="element in list"
                         :key="element.name"
-                      >
-                        {{ element.name }}
-                      </div>
-                    </draggable>
+                    >
+                      {{ element.name }}
+                    </div>
+                  </draggable>
                   <div class="option_edit_add_btn_wrap">
                     <button type="button" class="option_edit_add_btn02">저장</button>
                   </div>
@@ -112,79 +117,106 @@
                 </div>
                 <div class="option_edit_add_right">
                   <div class="option_edit_add_title_wrap">
-                  <h4 class="option_edit_add_title">카테고리 연결</h4>
+                    <h4 class="option_edit_add_title">카테고리 연결</h4>
                   </div>
-                  <button type="button" v-for="(a, i) in $store.state.CategoryOptionData" :key="i" class="option_edit_name">
+                  <button type="button" v-for="(a, i) in $store.state.CategoryOptionData" :key="i"
+                          class="option_edit_name">
                     {{ $store.state.CategoryOptionData[i].dessert }}
                     <p>{{ $store.state.CategoryOptionData[i].dessertname }}</p>
                   </button>
-                    <div class="option_edit_add_btn_wrap">
-                      <button type="button" class="option_edit_add_btn02">(2개선택){{}}연결</button>
-                    </div>                  
+                  <div class="option_edit_add_btn_wrap">
+                    <button type="button" class="option_edit_add_btn02">(2개선택){{}}연결</button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>         
+          </div>
         </label>
       </div>
     </div>
     <div class="menuadd_btn_con">
       <button type="button" class="menuadd_btn">메뉴추가</button>
-    </div>  
+    </div>
   </div>
 
 </template>
 
 <script>
-  import { defineComponent } from 'vue'
-  import { VueDraggableNext } from 'vue-draggable-next'
+import { defineComponent } from 'vue'
+import { VueDraggableNext } from 'vue-draggable-next'
+import axios from 'axios'
+
 export default {
-    components: {
-      draggable: VueDraggableNext,
+  components: {
+    draggable: VueDraggableNext,
+  },
+  data () {
+    return {
+      enabled: true,
+      list: [
+        {
+          name: 'HOT/ICED',
+          id: 1
+        },
+        {
+          name: '샷추가-기본2샷',
+          id: 2
+        },
+        {
+          name: '시럽추가선택',
+          id: 3
+        },
+        {
+          name: '추가 옵션',
+          id: 4
+        },
+      ],
+      dragging: false,
+      categoryAdd: false,
+      cgData: [],
+
+    }
+  },
+  methods: {
+    log (event) {
+      console.log(event)
     },
-      data() {
+    handleChange () {
+      console.log('changed')
+    },
+    inputChanged (value) {
+      this.activeNames = value
+    },
+    getComponentData () {
       return {
-        enabled: true,
-        list: [
-          { name: 'HOT/ICED', id: 1 },
-          { name: '샷추가-기본2샷', id: 2 },
-          { name: '시럽추가선택', id: 3 },
-          { name: '추가 옵션', id: 4 },
-        ],
-        dragging: false,
-        categoryAdd:false,
+        on: {
+          change: this.handleChange,
+          input: this.inputChanged
+        },
+        attrs: {
+          wrap: true
+        },
+        props: {
+          value: this.activeNames
+        }
       }
     },
-    methods: {
-        log(event) {
-        console.log(event)
-      },
-        handleChange() {
-        console.log('changed');
+    upload (e) {
+      var file = e.target.files
+      let url = URL.createObjectURL(file[0])
+      this.image = url
     },
-        inputChanged(value) {
-        this.activeNames = value;
-    },
-        getComponentData() {
-            return {
-                on: {
-                change: this.handleChange,
-                input: this.inputChanged
-                },
-                attrs:{
-                wrap: true
-                },
-                props: {
-                value: this.activeNames
-                }
-            };
-    },
-        upload(e){
-      var file = e.target.files;
-      let url = URL.createObjectURL(file[0]);
-      this.image = url;
-    },
-}
+    getCategory () {
+      axios.post('/apimenu/menucategory')
+          .then(res => {
+            this.cgData = res.data.rows
+          })
+    }
+  },
+  beforeMount () {
+    this.getCategory()
+    console.log(this.cgData)
+  }
 }
 </script>
 
