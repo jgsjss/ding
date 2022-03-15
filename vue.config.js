7ee816d517a8428b0460299893aa7b3b4ccb3107
liam <eetadev01@gmail.com>
2022-03-15 10:25:42 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 
 // const target = "http://192.168.0.110:5000";
-// const target = "http://39.113.242.19:5000";
-const target = "http://localhost:5000";
+const target = "http://39.113.242.19:5000";
+// const target = "http://localhost:5000";
 
 
 module.exports = {
