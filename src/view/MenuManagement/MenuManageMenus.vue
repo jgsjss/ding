194f30071bf539903dcd30c13c62fa51aa42340b
dcp <eetadev01@gmail.com>
2022-03-28 16:30:57 +0900

    Update MenuManageMenus.vue

--- src/view/MenuManagement/MenuManageMenus.vue
+++ src/view/MenuManagement/MenuManageMenus.vue
@@ -57,7 +57,7 @@
         </thead>
         <tbody>
         <tr v-for="(a, i) in menuData" :key="i">
-          <td v-for="menuname in a" :key="menuname"></td>
+          <td v-for="menuname in a" :key="i"></td>
           <td scope="row" class="cate_check_box">
             <input type="checkbox"
                    :id="'menuCheck_' + i.menuId"
