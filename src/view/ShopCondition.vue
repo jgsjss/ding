ee4a83ff44bd5fc52bb70a80d251e60fac470f2c
hayul2 <haeul1008@naver.com>
2022-04-07 10:43:34 +0900

    04.07 10:43
    d

--- src/view/ShopCondition.vue
+++ src/view/ShopCondition.vue
@@ -2,7 +2,9 @@
         <div class="shopCondition_wrap">
         <button class="main-btn" type="submit">
           <span class="condition">매장상태</span>
-          <span class="condition" >:영업중 {{}}</span>
+          <span 
+            class="condition"
+            >:영업중{{}}</span>
         </button>
         <i class="xi-bell"></i>
       </div> 
@@ -10,7 +12,13 @@
 
 <script>
 export default {
-
+  components: {
+  },
+  data() {
+    return {
+      CheckCondition:''
+    }
+  },
 }
 </script>
 
